0aa63e40e639038c254567b5da67c966df03673d
pbender <pbender@RRC.CA>
2025-10-01 12:51:30 -0500

    else if added to the renderQuestions

--- script.js
+++ script.js
@@ -29,12 +29,34 @@ document.addEventListener("DOMContentLoaded", () =>{
                 currentState = "scene3";
                 renderQuestion();
             };
+    // console.log(sceneTitle.textContent);
+    // console.log(sceneText.textContent);
+    // console.log(button1.style.display);
+    // console.log(button2.style.display);
+    // console.log(currentState);
+        } else if (currentState === "scene2") {
+            container.classList.add("scene2");
+            sceneTitle.textContent = "Okay... Enjoy your cold marshmallow.";
+            sceneText.textContent = "Delicious! Would you like another?";
+            button1.style.display = "inline-block";
+            button1.textContent = "More please";
+            button1.onclick = () => {
+                currentState = "scene1";
+                renderQuestion();
+            };
+            button2.style.display = "inline-block";
+            button2.textContent = "One was enough";
+            button2.onclick = () => {
+                currentState = "scene7";
+                renderQuestion();
+            };
     console.log(sceneTitle.textContent);
     console.log(sceneText.textContent);
     console.log(button1.style.display);
     console.log(button2.style.display);
     console.log(currentState);
-        }
+       
+        }     
     };
     renderQuestion();
 });
