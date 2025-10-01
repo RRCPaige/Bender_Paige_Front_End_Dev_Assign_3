5c0959e100852f5474536591e378d44645f0d08b
pbender <pbender@RRC.CA>
2025-10-01 09:26:17 -0500

    created constants for my html ids

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
+const sceneTitle = document.getElementById("scene-title");
+const sceneText = document.getElementById("scene-text");
+const button1 = document.getElementById("choice-1");
+const button2 = document.getElementById("choice-2");
+
+
 let currentState = "scene-1";
 
-const scenes = {
-    "scene-1": {
-        content: ''
-    }
-}
